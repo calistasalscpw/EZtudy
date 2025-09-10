599ce4219a122e02eb2047248e82b72fb3c7c9cc
Calista Candramawa <121235720+calistasalscpw@users.noreply.github.com>
2025-09-10 17:50:43 +0700

    fix: API failture and limit handling

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import passport from 'passport';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import rateLimit from 'express-rate-limit';
 
 import userRouter from './routers/user.js';
 import courseRouter from './routers/course.js';
@@ -24,6 +25,14 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+app.use(limiter);
 
 // Authentication middleware
 app.use((req, res, next) => {
